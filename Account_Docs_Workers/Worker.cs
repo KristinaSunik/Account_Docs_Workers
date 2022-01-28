4d5d98230511b96692001242abf21c928cc222b7
Кристина Сюник <anitanit43@gmail.com>
2022-01-28 21:16:33 +0300

    added class Worker

---
+++ Account_Docs_Workers/Worker.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Account_Docs_Workers
+{
+    class Worker
+    {
+        public string Name { get; private set; }
+        public string Surname { get; private set; }
+        public string Patronymic { get; private set; }
+        public DateTime BirthDay { get; private set; }
+
+        public Worker(string name, string surname, string patronymic, DateTime birthDay)
+        {
+            Name = name;
+            Surname = surname;
+            Patronymic = patronymic;
+            BirthDay = birthDay;
+        }
+    }
+}
