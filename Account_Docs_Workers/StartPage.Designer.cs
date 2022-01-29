3bfebcc66df5d4f8f90a0f9ef667f5f215dcd7a3
Кристина Сюник <anitanit43@gmail.com>
2022-01-29 13:40:02 +0300

    startPage disign

--- Account_Docs_Workers/StartPage.Designer.cs
+++ Account_Docs_Workers/StartPage.Designer.cs
@@ -30,14 +30,54 @@ namespace Account_Docs_Workers
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(StartPage));
+            this.WorkersListButton = new System.Windows.Forms.Button();
+            this.DocumentsListButton = new System.Windows.Forms.Button();
+            this.AddNewWorkerButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // WorkersListButton
+            // 
+            this.WorkersListButton.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.WorkersListButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.WorkersListButton.Location = new System.Drawing.Point(35, 50);
+            this.WorkersListButton.Name = "WorkersListButton";
+            this.WorkersListButton.Size = new System.Drawing.Size(484, 45);
+            this.WorkersListButton.TabIndex = 0;
+            this.WorkersListButton.Text = "Список работников";
+            this.WorkersListButton.UseVisualStyleBackColor = false;
+            // 
+            // DocumentsListButton
+            // 
+            this.DocumentsListButton.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.DocumentsListButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.DocumentsListButton.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.DocumentsListButton.Location = new System.Drawing.Point(35, 145);
+            this.DocumentsListButton.Name = "DocumentsListButton";
+            this.DocumentsListButton.Size = new System.Drawing.Size(484, 45);
+            this.DocumentsListButton.TabIndex = 1;
+            this.DocumentsListButton.Text = "Список документов";
+            this.DocumentsListButton.UseVisualStyleBackColor = false;
+            // 
+            // AddNewWorkerButton
+            // 
+            this.AddNewWorkerButton.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.AddNewWorkerButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.AddNewWorkerButton.Location = new System.Drawing.Point(35, 290);
+            this.AddNewWorkerButton.Name = "AddNewWorkerButton";
+            this.AddNewWorkerButton.Size = new System.Drawing.Size(484, 45);
+            this.AddNewWorkerButton.TabIndex = 2;
+            this.AddNewWorkerButton.Text = "Добавить нового работника";
+            this.AddNewWorkerButton.UseVisualStyleBackColor = false;
             // 
             // StartPage
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.MenuBar;
-            this.ClientSize = new System.Drawing.Size(1154, 755);
+            this.ClientSize = new System.Drawing.Size(580, 426);
+            this.Controls.Add(this.AddNewWorkerButton);
+            this.Controls.Add(this.DocumentsListButton);
+            this.Controls.Add(this.WorkersListButton);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "StartPage";
             this.Text = "Работники и выданные документы";
@@ -46,6 +86,10 @@ namespace Account_Docs_Workers
         }
 
         #endregion
+
+        private System.Windows.Forms.Button WorkersListButton;
+        private System.Windows.Forms.Button DocumentsListButton;
+        private System.Windows.Forms.Button AddNewWorkerButton;
     }
 }
 
